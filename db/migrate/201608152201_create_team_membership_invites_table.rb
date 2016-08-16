bbbdcc5f59b45bdf913fd29319ebb79f964b242e
Bernardo Araujo <bernardo.amc@gmail.com>
2016-08-15 22:44:43 -0400

    Add team_membership_requests and team_membership_invites tables.

---
+++ db/migrate/201608152201_create_team_membership_invites_table.rb
@@ -0,0 +1,25 @@
+class CreateTeamMembershipInvitesTable < ActiveRecord::Migration
+  def up
+    create_table :team_membership_invites do |t|
+      t.integer :team_id, null: false
+      t.integer :user_id, null: false
+      t.integer :inviter_id
+      t.boolean :refused, null: false, default: false
+      t.timestamps null: false
+    end
+
+    add_index :team_membership_invites, [:team_id, :user_id], unique: true
+
+    execute <<-SQL
+      INSERT INTO team_membership_invites(team_id, user_id, inviter_id, created_at, updated_at)
+      SELECT team_id, user_id, inviter_id, created_at, updated_at
+      FROM team_memberships
+      WHERE confirmed is FALSE
+    SQL
+  end
+
+
+  def down
+    drop_table :team_membership_invites
+  end
+end
---
+++ db/migrate/201608152223_create_team_membership_requests_table.rb
@@ -0,0 +1,12 @@
+class CreateTeamMembershipRequestsTable < ActiveRecord::Migration
+  def change
+    create_table :team_membership_requests do |t|
+      t.integer :team_id, null: false
+      t.integer :user_id, null: false
+      t.boolean :denied, null: false, default: false
+      t.timestamps null: false
+    end
+
+    add_index :team_membership_requests, [:team_id, :user_id], unique: true
+  end
+end
